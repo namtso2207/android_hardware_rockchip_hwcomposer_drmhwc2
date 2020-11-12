a00a4b57890c05b5e16c0e881928ce6015d04c22
libin <bin.li@rock-chips.com>
2020-11-12 10:45:11 +0800

    ==========update to version 1.1.0==========
    Signed-off-by: libin <bin.li@rock-chips.com>
    Change-Id: I72e0c8a2e2965d3a1ddfdef87af9c772b21566b6

--- rockchip/utils/drmdebug.cpp
+++ rockchip/utils/drmdebug.cpp
@@ -41,7 +41,7 @@
 #include "rockchip/utils/drmdebug.h"
 
 /*hwc version*/
-#define GHWC_VERSION                    "HWC2-1.0"
+#define GHWC_VERSION                    "HWC2-1.1.0"
 
 namespace android {
 
