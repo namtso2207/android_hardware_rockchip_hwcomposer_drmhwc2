fa437d854f3935be4181064648359ae4d6b6c53d
libin <bin.li@rock-chips.com>
2021-01-27 16:51:16 +0800

    Modify DrmPlane and DrmConnector binding relationship.
    Version: 1.1.74
    
    Signed-off-by: libin <bin.li@rock-chips.com>
    Change-Id: I33188b6d3294b7b962c256f84409f511ac9bea36

--- include/rockchip/utils/drmdebug.h
+++ include/rockchip/utils/drmdebug.h
@@ -41,7 +41,7 @@
 
 
 /*hwc version*/
-#define GHWC_VERSION                    "HWC2-1.1.73"
+#define GHWC_VERSION                    "HWC2-1.1.74"
 
 //Print call statck when you call ALOGD_CALLSTACK.
 #define ALOGD_CALLSTACK(...)                             \
