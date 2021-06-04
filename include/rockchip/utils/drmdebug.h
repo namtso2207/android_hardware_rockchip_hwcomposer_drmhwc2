8692f3ecedebc0a70c676eb6d6698949b3e828e7
libin <bin.li@rock-chips.com>
2021-06-04 14:11:50 +0800

    DrmHwc2 : HDR mode
    Optimize the process of switching HDR mode.
    
    Verison: 1.1.146
    
    Signed-off-by: libin <bin.li@rock-chips.com>
    Change-Id: I1e5bb36248cfcf5becf7d1931f9bd4367b4a5da6

--- include/rockchip/utils/drmdebug.h
+++ include/rockchip/utils/drmdebug.h
@@ -41,7 +41,7 @@
 
 
 /*hwc version*/
-#define GHWC_VERSION                    "HWC2-1.1.145"
+#define GHWC_VERSION                    "HWC2-1.1.146"
 
 //Print call statck when you call ALOGD_CALLSTACK.
 #define ALOGD_CALLSTACK(...)                             \
