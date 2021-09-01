c2771f5244157a4f11c706ef8cc0006702e406d2
libin <bin.li@rock-chips.com>
2021-09-01 16:49:00 +0800

    DrmHwc2 : Fix compiled error on Android S.
    Supplemental version.
    
    Verison: 1.1.170
    
    Signed-off-by: libin <bin.li@rock-chips.com>
    Change-Id: I48b76d653c36e13b13591e0d1b4f5fe22e26c1cb

--- include/rockchip/utils/drmdebug.h
+++ include/rockchip/utils/drmdebug.h
@@ -41,7 +41,7 @@
 
 
 /*hwc version*/
-#define GHWC_VERSION                    "HWC2-1.1.169"
+#define GHWC_VERSION                    "HWC2-1.1.170"
 
 //Print call statck when you call ALOGD_CALLSTACK.
 #define ALOGD_CALLSTACK(...)                             \
