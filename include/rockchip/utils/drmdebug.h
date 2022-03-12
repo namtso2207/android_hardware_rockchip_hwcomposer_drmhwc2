b00b0fe5a8810d8b66dcca505b307064b2b77368
libin <bin.li@rock-chips.com>
2022-03-12 18:00:00 +0800

    DrmVop3588 : Cluster support noAfbc-RGB format.
    Version : 1.2.77
    
    Signed-off-by: libin <bin.li@rock-chips.com>
    Change-Id: I752acd3ed2f2ebe65f6b8c0f8683ca1f309ad018

--- include/rockchip/utils/drmdebug.h
+++ include/rockchip/utils/drmdebug.h
@@ -41,7 +41,7 @@
 
 
 /*hwc version*/
-#define GHWC_VERSION                    "HWC2-1.2.76"
+#define GHWC_VERSION                    "HWC2-1.2.77"
 
 //Print call statck when you call ALOGD_CALLSTACK.
 #define ALOGD_CALLSTACK(...)                             \
