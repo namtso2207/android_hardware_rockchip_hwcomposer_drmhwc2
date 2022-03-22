f6295cdb91d229015ffebb93906c82b6ae232a86
libin <bin.li@rock-chips.com>
2022-03-22 19:01:06 +0800

    DrmVop3588 : Cluster can't rotate noAfbc format.
    Version : 1.2.78
    
    Signed-off-by: libin <bin.li@rock-chips.com>
    Change-Id: I0ab789a51216304eabdea8e2767210c890ac3c55

--- include/rockchip/utils/drmdebug.h
+++ include/rockchip/utils/drmdebug.h
@@ -41,7 +41,7 @@
 
 
 /*hwc version*/
-#define GHWC_VERSION                    "HWC2-1.2.77"
+#define GHWC_VERSION                    "HWC2-1.2.78"
 
 //Print call statck when you call ALOGD_CALLSTACK.
 #define ALOGD_CALLSTACK(...)                             \
